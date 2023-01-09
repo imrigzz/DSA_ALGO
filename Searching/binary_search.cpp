48f07622f12eaf9bd165c88b2bcdc1bd9342f317
imrigzz <rizinchuk@gmail.com>
2023-01-09 23:13:15 +0530

    binary search iterative completed

--- Searching/binary_search.cpp
+++ Searching/binary_search.cpp
@@ -3,9 +3,36 @@
 #include <vector>
 using namespace std;
 
+int binary_search_iterative(vector<int> vec, int x)
+{
+    int left, right, mid;
+    left = 0;
+    right = vec.size() - 1;
+
+    while (left <= right)
+    {
+        mid = (right - left / 2) + left;
+        if (vec[mid] == x)
+        {
+            return mid;
+        }
+        if (x < vec[mid])
+        {
+            right = mid - 1;
+        }
+        if (x > vec[mid])
+        {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     // your code here
-
+    vector<int> vec = {10, 10};
+    int x = 10;
+    cout << binary_search_iterative(vec, x);
     return 0;
 }
